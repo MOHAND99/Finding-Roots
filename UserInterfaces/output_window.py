d3a7214598691206097d1534ceb1e95c5b01019e
unknown <mzayton10@gmail.com>
2021-11-12 12:50:01 +0200

    py_file

--- UserInterfaces/output_window.py
+++ UserInterfaces/output_window.py
@@ -8,10 +8,16 @@ class OutputWindow(QtWidgets.QDialog):
     def __init__(self) -> None:
         super(OutputWindow, self).__init__()
         loadUi(r"UserInterfaces\output.ui", self)
+        self.iteration_value = self.findChild(QtWidgets.QLabel,"iteration_value")
+    
+    def set_iteration_value(self, v):
+        self.iteration_value.setText(v)
+
 
 
 app = QtWidgets.QApplication(sys.argv)
 OutputWindow = OutputWindow()
+OutputWindow.set_iteration_value("22")
 widget = QtWidgets.QStackedWidget()
 widget.addWidget(OutputWindow)
 widget.resize(700,700)
