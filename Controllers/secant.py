df5fff06479f241ff7dca7f69c6536a0587b78fd
mohamed-zayton <mzayton10@gmail.com>
2021-11-23 11:57:47 +0200

    modify

--- Controllers/secant.py
+++ Controllers/secant.py
@@ -1,16 +1,18 @@
 from sympy import parse_expr, symbols
-import math
+
+
 def secant_eq(equ, x_prev, x_curr, iter, prec):
     x = symbols("x")    
     it_dic = {}
     fn = parse_expr(equ)
     num_of_iter = 0
+    x_next = 0
     for i in range(iter):
         num_of_iter += 1
         f_x_curr = fn.subs(x, x_curr)
         f_x_prev = fn.subs(x, x_prev)
         x_next = x_curr - ((f_x_curr * (x_prev - x_curr))/(f_x_prev - f_x_curr))
-        calc_prec = math.Abs((x_next-x_curr)/x_next) * 100
+        calc_prec = abs((x_next-x_curr)/x_next) * 100
         if calc_prec < prec:
             it_dic[i] = [x_next, x_curr, x_prev, calc_prec]
             break
@@ -19,7 +21,7 @@ def secant_eq(equ, x_prev, x_curr, iter, prec):
         x_curr = x_next
 
 
-    return[it_dic, equ, iter, prec, num_of_iter]
+    return[it_dic, x_next, equ, iter, prec, num_of_iter]
 
 
 
