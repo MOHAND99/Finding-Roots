97a6be28c8bf4fe99232bd831cffdedf7a2eaf1d
mohamed-zayton <mzayton10@gmail.com>
2021-11-29 16:43:39 +0200

    v5

--- Algorithm/newton_raphson.py
+++ Algorithm/newton_raphson.py
@@ -1,28 +1,29 @@
 from sympy import parse_expr, symbols, diff
 
 
-def newton_eq(equ, x_curr, iter=50, prec=0.00001):
+def newton_eq(equ, x_curr, MAX_ITERS=50, prec=0.00001):
     x = symbols("x")    
-    it_dic = {}
+    iters_data = {}
     fn = parse_expr(equ)
     fn_d = diff(fn, x)
     num_of_iter = 0
     x_next = 0
+    
     if fn_d == 0:
         return "Error"
         
-    calc_prec = 0
+    calc_prec = 0   
     for i in range(iter):
         f_x_curr = fn.subs(x, x_curr)
         f_x_derv = fn_d.subs(x, x_curr)
-        x_next = round(x_curr - (f_x_curr / f_x_derv), 6)
+        round_digit = 6
+        x_next = x_curr - (f_x_curr / f_x_derv)
         calc_prec = abs((x_next-x_curr)/x_next) * 100
+        iters_data[i] = [round(x_curr, round_digit), round(x_next, round_digit),calc_prec]
         if calc_prec < prec:
             num_of_iter = i
-            it_dic[i] = [x_curr, x_next, calc_prec]
             break
-        it_dic[i] = [x_curr, x_next, calc_prec]
         x_curr = x_next
 
 
-    return [it_dic, x_next, equ, iter, prec, num_of_iter,'newton']
+    return [iters_data, x_next, equ, MAX_ITERS, prec, num_of_iter+1,'newton']
