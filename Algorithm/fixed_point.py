1ce2a16c6c892c01fe6d4aec8749dd0bd147f8d2
mohamed-zayton <47240722+mohamed-zayton@users.noreply.github.com>
2021-11-29 22:40:58 +0200

    v1 (#14)

--- Algorithm/fixed_point.py
+++ Algorithm/fixed_point.py
@@ -2,19 +2,19 @@ from sympy import parse_expr, symbols
 
 def fixed_pt(equ, x_curr, MAX_ITERS=50, prec=0.00001):
     x = symbols("x")    
-    iters_data = {}
+    iters_data = []
     fn = parse_expr(equ)
     num_of_iter = MAX_ITERS
     x_next = 0
-
-        
+    iters_data.append(['Xi', 'Xi+1' , 'Precession'])
     calc_prec = 0   
+    
     for i in range(MAX_ITERS):
         f_x_curr = fn.subs(x, x_curr)
         round_digit = 6
         x_next = f_x_curr
         calc_prec = abs((x_next-x_curr)/x_next) * 100
-        iters_data[i] = [round(x_curr, round_digit), round(x_next, round_digit), calc_prec]
+        iters_data.append([round(x_curr, round_digit), round(x_next, round_digit), calc_prec])
         if calc_prec < prec:
             num_of_iter = i + 1
             break
