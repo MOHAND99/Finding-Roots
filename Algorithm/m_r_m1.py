f871c49b0744541f2c0fae5e36f81d5d4f52db3a
mohamed-zayton <mzayton10@gmail.com>
2021-11-29 22:39:37 +0200

    v1

--- Algorithm/m_r_m1.py
+++ Algorithm/m_r_m1.py
@@ -3,7 +3,7 @@ from sympy import parse_expr, symbols, diff
 
 def multiple_root_mod_one(equ, x_curr, m, MAX_ITERS=50, prec=0.00001):
     x = symbols("x")    
-    iters_data = {}
+    iters_data = []
     fn = parse_expr(equ)
     fn_derv = diff(fn, x)
     num_of_iter = MAX_ITERS
@@ -11,7 +11,7 @@ def multiple_root_mod_one(equ, x_curr, m, MAX_ITERS=50, prec=0.00001):
 
     if fn_derv == 0:
         return "Error First Derv. Is Zero"
-
+    iters_data.append(['Xi', 'Xi+1' , 'Precession'])
     calc_prec = 0   
     for i in range(MAX_ITERS):
         f_x_curr = fn.subs(x, x_curr)
@@ -19,7 +19,7 @@ def multiple_root_mod_one(equ, x_curr, m, MAX_ITERS=50, prec=0.00001):
         round_digit = 6
         x_next = x_curr - (m * (f_x_curr / f_x_derv))
         calc_prec = abs((x_next-x_curr)/x_next) * 100
-        iters_data[i] = [round(x_curr, round_digit), round(x_next, round_digit),calc_prec]
+        iters_data.append([round(x_curr, round_digit), round(x_next, round_digit),calc_prec])
         if calc_prec < prec:
             num_of_iter = i + 1
             break
--- Algorithm/m_r_m2.py
+++ Algorithm/m_r_m2.py
@@ -3,7 +3,7 @@ from sympy import parse_expr, symbols, diff
 
 def multiple_root_mod_two(equ, x_curr, MAX_ITERS=50, prec=0.00001):
     x = symbols("x")    
-    iters_data = {}
+    iters_data = []
     fn = parse_expr(equ)
     fn_derv = diff(fn, x)
     fn_second_derv = diff(fn_derv, x)
@@ -16,6 +16,8 @@ def multiple_root_mod_two(equ, x_curr, MAX_ITERS=50, prec=0.00001):
     if fn_second_derv == 0:
         return "Error Second Derv. Is Zero"
 
+    iters_data.append(['Xi', 'Xi+1' , 'Precession'])
+
     calc_prec = 0   
     for i in range(MAX_ITERS):
         f_x_curr = fn.subs(x, x_curr)
@@ -24,7 +26,7 @@ def multiple_root_mod_two(equ, x_curr, MAX_ITERS=50, prec=0.00001):
         round_digit = 6
         x_next = x_curr - ((f_x_curr * f_x_derv) / ((f_x_derv**2)-(f_x_curr * f_x_sec_derv)))
         calc_prec = abs((x_next-x_curr)/x_next) * 100
-        iters_data[i] = [round(x_curr, round_digit), round(x_next, round_digit),calc_prec]
+        iters_data.append([round(x_curr, round_digit), round(x_next, round_digit),calc_prec])
         if calc_prec < prec:
             num_of_iter = i + 1
             break
