69e368f071df86f70e1477a9a910128c95dc82d5
mohamed-zayton <mzayton10@gmail.com>
2021-11-29 21:08:36 +0200

    v4

--- UserInterfaces/output_window.py
+++ UserInterfaces/output_window.py
@@ -12,33 +12,17 @@ class OutputWindow(QtWidgets.QDialog):
         self.root_val = self.findChild(QtWidgets.QLabel, "root_value")
         self.precision = self.findChild(QtWidgets.QLabel, "precision_value")
 
-    def set_table_vals(self, data, type):
+    def set_table_vals(self, data):
         col_count = len(data[0])
         row_count = len(data) - 1
         self.table.setRowCount(row_count)
         self.table.setColumnCount(col_count)
-        self.table.setHorizontalHeaderLabels(data[0])
-        if type == "secant":       
+        self.table.setHorizontalHeaderLabels(data[0])      
+        for i in range(row_count+1):
             col_ind = 0
-            for i in range(row_count+1):
-                self.table.setItem(i,col_ind,QtWidgets.QTableWidgetItem(str(data[i+1][0])))
-                self.table.setItem(i,col_ind+1,QtWidgets.QTableWidgetItem(str(data[i+1][1])))
-                self.table.setItem(i,col_ind+2,QtWidgets.QTableWidgetItem(str(data[i+1][2])))
-                self.table.setItem(i,col_ind+3,QtWidgets.QTableWidgetItem(str(data[i+1][3])))
-            
-            self.table.resizeColumnsToContents()
-        elif type == "falseposition":
-            pass
-        elif type == "newton":
-            pass
-        elif type == "fixed_pt":
-            pass
-        elif type == "biesection":
-            pass 
-        elif type == "mr_first":
-            pass
-        else:
-            pass
+            for j in range(col_count):    
+                self.table.setItem(i,col_ind+j,QtWidgets.QTableWidgetItem(str(data[i+1][j])))
+        self.table.resizeColumnsToContents()
        
 
     def set_iteration_value(self, v):      
@@ -49,3 +33,21 @@ class OutputWindow(QtWidgets.QDialog):
 
     def set_prec_val(self, v):
         self.precision.setText(v)
+
+
+                # self.table.setItem(i,col_ind+1,QtWidgets.QTableWidgetItem(str(data[i+1][1])))
+                # self.table.setItem(i,col_ind+2,QtWidgets.QTableWidgetItem(str(data[i+1][2])))
+                # self.table.setItem(i,col_ind+3,QtWidgets.QTableWidgetItem(str(data[i+1][3])))
+            
+        # elif type == "falseposition":
+        #     pass
+        # elif type == "newton":
+        #     pass
+        # elif type == "fixed_pt":
+        #     pass
+        # elif type == "biesection":
+        #     pass 
+        # elif type == "mr_first":
+        #     pass
+        # else:
+        #     pass
