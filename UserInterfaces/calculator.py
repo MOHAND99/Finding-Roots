5b88894c35c0f3f5d309fbf99936fd1337ddc8fa
MOHAND99 <muhanned122000@yahoo.com>
2021-11-29 08:43:00 +0200

    add-calculator-ui

---
+++ UserInterfaces/calculator.py
@@ -0,0 +1,20 @@
+from PyQt5.uic import loadUi
+from PyQt5.QtWidgets import QMainWindow, QApplication
+import sys
+
+
+class Calculator(QMainWindow):
+    def __init__(self):
+        super(Calculator, self).__init__()
+        loadUi('calculator.ui', self)
+
+        self.cosine.clicked.connect(self.__add_to_output("cos("))
+        self.show()
+
+    def __add_to_output(self, text):
+        self.outputlabel.setText(self.outputlabel.text + text + " ")
+
+
+app = QApplication(sys.argv)   # Create an instance of QtWidgets.QApplication
+window = Calculator()   # Create an instance of our class
+app.exec_()
