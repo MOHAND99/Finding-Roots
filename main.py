abace0de91c1732c240ae966272857ba5231f54c
MOHAND99 <muhanned122000@yahoo.com>
2021-12-05 16:23:39 +0200

    linting and data class

--- main.py
+++ main.py
@@ -3,11 +3,19 @@ import os.path
 sys.path.append(os.path.dirname(__file__) + r'\UserInterfaces')
 from PyQt5 import QtWidgets
 from UserInterfaces.entry_way import Ui_Form
+from dataclasses import dataclass
 
 
+@dataclass
+class Data:
+    equation: str = ""
+    method: str = ""
+
+
+data = Data()
 app = QtWidgets.QApplication(sys.argv)
 Form = QtWidgets.QWidget()
 ui = Ui_Form()
-ui.setupUi(Form)
+ui.setupUi(Form, data)
 Form.show()
 sys.exit(app.exec_())
