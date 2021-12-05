31d907dee04f8962942446604dc9f6b3a261e098
Muhannad Muhammad Bashar <53486163+MOHAND99@users.noreply.github.com>
2021-12-05 16:30:16 +0200

    Fix paths (#22)
    * sys path
    
    * linting and data class

--- main.py
+++ main.py
@@ -1,18 +1,24 @@
 import sys
 import os.path
-print(os.path.dirname(__file__))
 if '/' in os.path.dirname(__file__):
     sys.path.append(os.path.dirname(__file__) + r'/UserInterfaces')
 else:
     sys.path.append(os.path.dirname(__file__) + r'\UserInterfaces')
-
 from PyQt5 import QtWidgets
 from UserInterfaces.entry_way import Ui_Form
+from dataclasses import dataclass
 
 
+@dataclass
+class Data:
+    equation: str = ""
+    method: str = ""
+
+
+data = Data()
 app = QtWidgets.QApplication(sys.argv)
 Form = QtWidgets.QWidget()
 ui = Ui_Form()
-ui.setupUi(Form)
+ui.setupUi(Form, data)
 Form.show()
 sys.exit(app.exec_())
